92caf876a6ca30c0bef66ce19cec96b974ef5ab6
Ubuntu <ubuntu@ip-172-31-37-101.us-east-2.compute.internal>
2021-05-18 03:27:38 +0000

    Django

--- basicforms/settings.py
+++ basicforms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q%ira+6@)suqfst1z=#yfai$5v36dt2gw0_^)$$-byt_q#bqrr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
